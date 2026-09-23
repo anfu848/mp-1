d9fbf7ef93950b3c63627fea5a42c5dae1892504
Ash Fu <anfu848@gmail.com>
2026-09-22 20:03:46 -0400

    Commit for MP-1, includes all requirements

---
+++ calc.js
@@ -0,0 +1,63 @@
+function addition() {
+    let first = Number(document.getElementById("first-num").value);
+    let second = Number(document.getElementById("second-num").value);
+    let result = first + second;
+    document.getElementById("output").innerHTML = String(result);
+}
+
+function subtraction() {
+    let first = Number(document.getElementById("first-num").value);
+    let second = Number(document.getElementById("second-num").value);
+    let result = first - second;
+    document.getElementById("output").innerHTML = String(result);
+    if (result < 0) {
+        document.getElementById("output").style.color = "red";
+    } else {
+        document.getElementById("output").style.color = "black";
+    }
+}
+
+function multiplication() {
+    let first = Number(document.getElementById("first-num").value);
+    let second = Number(document.getElementById("second-num").value);
+    let result = first * second;
+    document.getElementById("output").innerHTML = String(result);
+    if (result < 0) {
+        document.getElementById("output").style.color = "red";
+    } else {
+        document.getElementById("output").style.color = "black";
+    }
+}
+
+function division() {
+    let first = Number(document.getElementById("first-num").value);
+    let second = Number(document.getElementById("second-num").value);
+    let result = first / second;
+    document.getElementById("output").innerHTML = String(result);
+    if (result < 0) {
+        document.getElementById("output").style.color = "red";
+    } else {
+        document.getElementById("output").style.color = "black";
+    }
+}
+
+function exponentiation() {
+    let first = Number(document.getElementById("first-num").value);
+    let second = Number(document.getElementById("second-num").value);
+    let result = 1;
+    for (let i = 0; i < second; i++) {
+        result = first * result;
+    }
+    document.getElementById("output").innerHTML = String(result);
+    if (result < 0) {
+        document.getElementById("output").style.color = "red";
+    } else {
+        document.getElementById("output").style.color = "black";
+    }
+}
+
+function calc_clear() {
+    document.getElementById("first-num").value = "";
+    document.getElementById("second-num").value = "";
+    document.getElementById("output").innerHTML = "";
+}
